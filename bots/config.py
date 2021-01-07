1a5a580bb96fa4d6bb2d9af1dda9787d2cee5d10
OreYusuf <52608450+OreYusuf@users.noreply.github.com>
2021-01-07 13:12:02 +0000

    Use of environment variables

---
+++ bots/config.py
@@ -0,0 +1,25 @@
+import tweepy
+import logging
+import os
+
+logger = logging.getLogger()
+logging.getLogger().setLevel(logging.INFO)
+
+def create_api():
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = ("CONSUMER_SECRET")
+    access_token = ("ACCESS_TOKEN")
+    access_token_secret = ("ACCESS_TOKEN_SECRET")
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, 
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+
+    return api
